1a9accb48d02c64f641b36da63f02b7087eb2d55
MdRoki2020 <mroki815@gmail.com>
2022-07-04 01:00:43 +0600

    working on server side

---
+++ server/models/Posts.js
@@ -0,0 +1,18 @@
+module.exports=(sequelize,DataTypes)=>{
+    const posts=sequelize.define("posts",{
+        title:{
+            type:DataTypes.STRING,
+            allowNull:false,
+        },
+        postText:{
+            type:DataTypes.STRING,
+            allowNull:false,
+        },
+        username:{
+            type:DataTypes.STRING,
+            allowNull:false,
+        }
+    });
+    
+    return posts;
+}
